6a1b66e6ea4db7310587ec468d965f7227c6eb10
VladosALAD <alalad_02@mail.ru>
2022-02-11 02:48:47 +0400

    Task 2

--- index.php
+++ index.php
@@ -10,4 +10,16 @@ $order .= " Hello World!";
 
 //// Don't change the line below
 
-echo "\nYour order is:$very_bad_unclear_name.";
+echo "<br>Your order is:$very_bad_unclear_name.";
+
+//Task 2
+$int1 = 10;
+echo $int1 . "<br>";
+$int2 = 1001;
+echo $int2;
+$float1 = 123.456;
+echo (6+6) . "<br>";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo ($last_month - $this_month);
